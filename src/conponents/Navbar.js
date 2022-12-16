8951eafe4ad39b0dba8cc55180f3940a92d645c5
Jay-E <84008602+holderj-dev@users.noreply.github.com>
2022-12-16 09:27:56 +0000

    12/16/22-1

--- src/conponents/Navbar.js
+++ src/conponents/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar() {
       <div className="bg-[#080808] rounded-md shadow ">
         <div className="flex items-stretch">
           <img src='https://halberdbastion.com/sites/default/files/styles/medium/public/2019-04/Orange-Liberia_logo.png?itok=MEhqibKc' width={30} height={30} alt="orange" />
-          <img className="align-right" src='https://i0.wp.com/lr.careerical.com/wp-content/uploads/2022/10/Lonestar-Cell-MTN-192x192-1.jpg?resize=225%2C225&ssl=1
+          <img className="right-90" src='https://i0.wp.com/lr.careerical.com/wp-content/uploads/2022/10/Lonestar-Cell-MTN-192x192-1.jpg?resize=225%2C225&ssl=1
 ' width={30} height={30} alt="lonestar" />
         </div>
         <div className="font-bold flex justify-center">
